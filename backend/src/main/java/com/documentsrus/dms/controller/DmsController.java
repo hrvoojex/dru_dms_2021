e54b3e977874fbcd887da798de490f999978062f
Hrvoje T <hrvooje@gmail.com>
2021-06-14 10:06:17 +0200

    Added ServiceResult

--- backend/src/main/java/com/documentsrus/dms/controller/DmsController.java
+++ backend/src/main/java/com/documentsrus/dms/controller/DmsController.java
@@ -1,7 +1,9 @@
 package com.documentsrus.dms.controller;
 
 import com.documentsrus.dms.model.Document;
+import com.documentsrus.dms.model.ServiceResult;
 import com.documentsrus.dms.service.DmsServiceInterface;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,26 +18,33 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/docs")
+@Slf4j
 public class DmsController {
 
     @Autowired
     DmsServiceInterface dmsService;
 
     @GetMapping("/get-document")
-    public ResponseEntity<Document> getDocument(@RequestParam("id") int id) {
+    public ResponseEntity getDocument(@RequestParam("id") int id) {
+        ServiceResult result = new ServiceResult();
+        Document document = new Document();
         try {
-            Document document = dmsService.getDocument(id);
-            return new ResponseEntity<>(
-                    document,
-                    HttpStatus.OK
-            );
+            document = dmsService.getDocument(id);
+        } catch (IndexOutOfBoundsException e) {
+            e.printStackTrace();
+            result.setMessage("Could not get document with id: " + id + " Resultset list is empty");
+            result.setResult(e.getMessage());
+            return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
         } catch (Exception e){
-            return new ResponseEntity<>(
-                    new Document(),
-
-                    HttpStatus.BAD_REQUEST
-            );
+            e.printStackTrace();
+            result.setMessage("Error getting document with id: " + id);
+            result.setResult(e.getMessage());
+            return new ResponseEntity<>(result, HttpStatus.BAD_REQUEST);
         }
+
+        result.setMessage("Document available");
+        result.setResult(document);
+        return new ResponseEntity<>(result, HttpStatus.OK);
     }
 
 }
---
+++ backend/src/main/java/com/documentsrus/dms/model/ServiceResult.java
@@ -0,0 +1,31 @@
+package com.documentsrus.dms.model;
+
+public class ServiceResult {
+
+    private String message;
+    private Object result;
+
+    public ServiceResult() {}
+
+    public ServiceResult(String message, Object result) {
+        this.message = message;
+        this.result = result;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Object getResult() {
+        return result;
+    }
+
+    public void setResult(Object result) {
+        this.result = result;
+    }
+
+}
